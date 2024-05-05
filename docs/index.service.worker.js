79557062af5c75c60e021cb9d067fd25387991c5
karsonatorz <karsondahlman@aol.com>
2024-05-05 15:15:01 -0600

    new

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1713760106|1783574739";
+const CACHE_VERSION = "1714943497|104792851";
 const CACHE_PREFIX = "Mountain-high-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
